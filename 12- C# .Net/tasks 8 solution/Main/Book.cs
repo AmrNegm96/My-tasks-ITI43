7fae0cbd7a24f2f9ff21ea20b4df80020e6711a4
Amr Negm <115468688+amrNegm11@users.noreply.github.com>
2023-01-30 23:30:02 +0200

    add task 8 solution
    Delegates

---
+++ 12- C# .Net/tasks 8 solution/Main/Book.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book() { }
+        public Book(string _ISBN, string _Title,
+        string[] _Authors, DateTime _PublicationDate,
+        decimal _Price)
+        {
+            ISBN= _ISBN;
+            Title= _Title;
+            Authors= _Authors;  
+            PublicationDate= _PublicationDate;
+            Price= _Price;
+        }
+        public override string ToString()
+        {
+            return $"""
+                    The book title: {this.Title}
+                    , its ISBN: {this.ISBN} 
+                     and its price is: {this.Price} EGP
+                    """;
+        }
+    }
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+        public static string GetAuthors(Book B)
+        {
+            StringBuilder SB = new StringBuilder();
+
+            foreach(string item in B.Authors)
+            {
+                SB.Append(item);
+                SB.Append(",");
+
+            }
+            SB.Remove(SB.Length-1,1);
+            return SB.ToString();
+        }
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString();
+        }
+    }
+    public class LibraryEngine
+    {
+        public delegate string BookDelDT (Book B);
+
+        public static void ProcessBooks(List<Book> bList , BookDelDT fPtr )
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr?.Invoke(B)?? "-1");
+            }
+        }
+        public static void ProcessBooksV2(List<Book> bList, Func <Book , string> fPtr )
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr?.Invoke(B) ?? "-1");
+            }
+        }
+    }
+}
---
+++ 12- C# .Net/tasks 8 solution/Main/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Book B1 = new Book("1236666", "A", new string[] { "a", "b", "c" }, dateTime, 200);
+            Book B2 = new Book("345", "B", new string[] { "a", "b", "c" }, dateTime, 300);
+            Book B3 = new Book("567", "C", new string[] { "a", "b", "c" }, dateTime, 500);
+            Book B4 = new Book("891", "D", new string[] { "a", "b", "c" }, dateTime, 300);
+            Book B5 = new Book("568", "E", new string[] { "a", "b", "c" }, dateTime, 100);
+
+            List<Book> list = new List<Book>();
+            list.Add(B1);
+            list.Add(B2);
+            list.Add(B3);
+            list.Add(B4);
+            list.Add(B5);
+
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooks(list, BookFunctions.GetTitle);
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooks(list, BookFunctions.GetAuthors);
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooks(list, BookFunctions.GetPrice);
+            Console.WriteLine("////////////////////");
+
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooksV2(list, BookFunctions.GetTitle);
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooksV2(list, BookFunctions.GetAuthors);
+            Console.WriteLine("////////////////////");
+            LibraryEngine.ProcessBooksV2(list, BookFunctions.GetPrice);
+            Console.WriteLine("////////////////////");
+
+
+            Func<Book , string> fPtr2 = delegate (Book b) { return b.ISBN; };
+            Console.WriteLine(fPtr2(B1));
+            Console.WriteLine("////////////////////");
+
+            Func<Book, DateTime> fPtr3 =  b => b.PublicationDate;
+            Console.WriteLine(fPtr3(B1));
+
+
+        }
+    }
+}
