625c882aff6dd923f9af7762252c6e5e8bac06d0
Amr Negm <115468688+amrNegm11@users.noreply.github.com>
2023-03-07 23:02:47 +0200

    added part 3 of task 16

---
+++ 12- C# .Net/tasks 16 solution/myReverseEngineering/MyContext/MyContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myReverseEngineering.MyConfigurations;
+using myReverseEngineering.MyEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myReverseEngineering.MyContext
+{
+    public partial class MyContext : DbContext
+    {
+        public MyContext() 
+        { 
+        
+        }
+        public MyContext(DbContextOptions<MyContext> options) 
+            : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Northwind;Integrated Security=True;Encrypt=false");
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        public virtual DbSet<Category> Categories { get; set; }
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfigurations());
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
